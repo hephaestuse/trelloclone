b48b8ec93b1814e91e331b4e274807d82b0758cd
ahad-mohammadi <mohammadiahad93@gmail.com>
2024-12-17 19:50:07 +0330

    lider for cards list in dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,22 +4,26 @@ import { useQuery } from "@tanstack/react-query";
 import { getBoards } from "../services/boards";
 import { useSelector } from "react-redux";
 import { RootState } from "../store";
-
+import CircularProgress from "@mui/material/CircularProgress";
 function Dashboard() {
   const userId = useSelector((state: RootState) => state.user.userId);
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["boards"],
     queryFn: () => getBoards(userId),
   });
   return (
     <Container>
+      {isLoading && (
+        <CircularProgress size={100} sx={{ display: "flex", margin: "auto" }} />
+      )}
+
       <Grid2 container spacing={1}>
         {data?.map((board) => (
           <BoardCard key={board.board_id} imgUrl={board.bg_Img}>
             {board.name}
           </BoardCard>
         ))}
-        <BoardCard type="plus"></BoardCard>
+        {isLoading || <BoardCard type="plus"></BoardCard>}
       </Grid2>
     </Container>
   );
