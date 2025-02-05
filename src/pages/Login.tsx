5fd3a8bd589afb872cb7227287ef8a2f02b98a1f
ahad-mohammadi <mohammadiahad93@gmail.com>
2025-02-05 15:45:27 +0330

    extract log in to componrnt

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,48 +1,11 @@
-import React, { useEffect, useState } from "react";
-import {
-  TextField,
-  Button,
-  Container,
-  Typography,
-  Box,
-} from "@mui/material";
-import { useMutation } from "@tanstack/react-query";
-import { loginUser } from "../services/auth";
-import { useNavigate } from "react-router-dom";
-import CircularProgress from "@mui/material/CircularProgress";
-import { useDispatch } from "react-redux";
-import { setUserId } from "../features/user/userSlice";
+import React, { useEffect } from "react";
+import { Container, Typography, Box } from "@mui/material";
+import LoginForm from "../components/LoginForm";
+
 const Login: React.FC = () => {
-  const [email, setEmail] = useState("xxx@xxx.com");
-  const [password, setPassword] = useState("123123");
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
   useEffect(() => {
     document.body.style.backgroundImage = "";
   }, []);
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Handle login logic here
-    console.log("Email:", email);
-    console.log("Password:", password);
-    mutate({ email, password });
-  };
-  const {
-    mutate,
-    isPending: isLoading,
-    isError,
-  } = useMutation({
-    mutationFn: loginUser,
-    onSuccess: (data) => {
-      console.log("Login successful:", data);
-
-      dispatch(setUserId(data.user.id));
-      navigate("/dashboard");
-    },
-    onError: (error) => {
-      console.error("Login error:", error.message);
-    },
-  });
 
   return (
     <>
@@ -79,74 +42,7 @@ const Login: React.FC = () => {
           </Typography>
         </Box>
         <Box sx={{ width: "50%", display: "flex", justifyContent: "center" }}>
-          <Box
-            sx={{
-              backgroundColor: "rgba(255, 255, 255, 0.8)",
-              padding: 4,
-              borderRadius: 2,
-
-              width: "50%", // Set the container to 50% of the page
-            }}
-          >
-            <Typography variant="h4" component="h1" gutterBottom>
-              Login
-            </Typography>
-            <form onSubmit={handleSubmit}>
-              <Box sx={{ mb: 2 }}>
-                <TextField
-                  fullWidth
-                  label="Email"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </Box>
-              <Box sx={{ mb: 2 }}>
-                <TextField
-                  fullWidth
-                  label="Password"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </Box>
-              <Box sx={{ mb: 2 }}>
-                {isError && (
-                  <Typography
-                    sx={{
-                      color: "error.dark",
-                      borderRadius: "5px",
-                      padding: "0.3rem",
-                      backgroundColor: "error.light",
-                    }}
-                  >
-                    Invalid Email or Password
-                  </Typography>
-                )}
-              </Box>
-              <Button
-                type="submit"
-                variant="contained"
-                color="primary"
-                fullWidth
-                disabled={isLoading}
-              >
-                {isLoading ? (
-                  <>
-                    wait
-                    <CircularProgress
-                      sx={{ marginX: "0.25rem" }}
-                      size={"1.5rem"}
-                    />
-                  </>
-                ) : (
-                  "Login"
-                )}
-              </Button>
-            </form>
-          </Box>
+          <LoginForm />
         </Box>
       </Container>
     </>
